3b87cc1d130c759587bda38cf1a2934b7f576b24
alamahul <118677015+alamahul@users.noreply.github.com>
2025-10-29 19:07:45 +0700

    finish project

---
+++ src/scripts/sw.js
@@ -0,0 +1,87 @@
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { NetworkFirst, StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { BASE_URL } from './config'; 
+
+// Do precaching
+const manifest = self.__WB_MANIFEST;
+precacheAndRoute(manifest);
+
+// Runtime caching
+registerRoute(
+  ({ url }) => {
+    return url.origin === 'https://font.googleapis.com' || url.origin === 'https://fonts.gstatic.com';
+  },
+  new CacheFirst({
+    cacheName: 'google-fonts',
+  })
+)
+
+registerRoute(
+  ({ url }) => {
+    return url.origin === 'https://cdnjs.cloudflare.com' || url.origin.includes('fontawesome');
+  },
+  new CacheFirst({
+    cacheName: 'fontawesome',
+  })
+)
+
+registerRoute(
+  ({ url }) => {
+    return url.origin === 'https://ui-avatars.com';
+  },
+  new CacheFirst({
+    cacheName: 'avatars-api',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+)
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(BASE_URL);
+    return baseUrl.origin === url.origin && request.destination !== 'image';
+  },
+  new NetworkFirst({
+    cacheName: 'citycare-api',
+  })
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(BASE_URL);
+    return baseUrl.origin === url.origin && request.destination === 'image';
+  },
+  new StaleWhileRevalidate({
+    cacheName: 'citycare-api',
+  }),
+);
+
+registerRoute(
+  ({ url }) => {
+    return url.origin.includes('maptiler');
+  },
+  new CacheFirst({
+    cacheName: 'maptiler',
+  })
+)
+
+
+
+self.addEventListener('push', (event) => {
+  console.log('[Service worker] pushing...');
+
+  async function showNotification() {
+    const data = await event.data.json();
+
+    await self.registration.showNotification(data.title, {
+      body: data.options.body,
+    });
+  }
+
+  event.waitUntil(showNotification());
+});
